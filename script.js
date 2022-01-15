7e6b855eca93a42482887834ec264af7d40dcc05
emyllesouza <emylima04@gmail.com>
2022-01-15 17:50:56 -0300

    adiciona botao e variaveis do personagem

---
+++ script.js
@@ -0,0 +1,6 @@
+const personagemImg = document.querySelector('#img');
+const personagemNome = document.querySelector('#nome');
+const personagemEspecie = document.querySelector('#especie');
+const personagemStatus = document.querySelector('#status');
+const botao = document.querySelector('#botao');
+
