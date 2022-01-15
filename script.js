10e17626cce05d2a9cad673537f89feb98b56168
emyllesouza <emylima04@gmail.com>
2022-01-15 18:04:49 -0300

    adiciona chamada a api e adiciona id no html

--- script.js
+++ script.js
@@ -4,3 +4,23 @@ const personagemEspecie = document.querySelector('#especie');
 const personagemStatus = document.querySelector('#status');
 const botao = document.querySelector('#botao');
 
+function buscaPersonagem() {
+    return fetch(`https://rickandmortyapi.com/api/character/1`, {
+        method: 'GET',
+        headers: {
+            Accept: 'application/json',
+            "Content-type": 'application/json'
+        }
+    }).then((response) => response.json()).then((data) => {
+        personagemImg.src = data.image;
+        console.log(data.image);
+        personagemImg.alt = data.name;
+        personagemNome.innerHTML = data.name;
+        personagemEspecie.innerHTML = data.species;
+        personagemStatus.innerHTML = data.status;
+    });
+}
+
+botao.addEventListener('click', () => {
+    buscaPersonagem();
+});
