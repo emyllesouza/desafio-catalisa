f53f32d9dd9f0a512011a1b99c3f2f7f9716a24d
emyllesouza <emylima04@gmail.com>
2022-01-15 23:23:20 -0300

    adiciona busca de tres personagens

--- script.js
+++ script.js
@@ -1,28 +1,67 @@
-const personagemImg = document.querySelector('#img');
-const personagemNome = document.querySelector('#nome');
-const personagemEspecie = document.querySelector('#especie');
-const personagemStatus = document.querySelector('#status');
+// Primeiro personagem
+const personagemUmImg = document.querySelector('#img-1');
+const personagemUmNome = document.querySelector('#personagem-um-nome');
+const personagemUmEspecie = document.querySelector('#personagem-um-especie');
+const personagemUmStatus = document.querySelector('#personagem-um-status');
+
+// Segundo personagem
+const personagemDoisImg = document.querySelector('#img-2');
+const personagemDoisNome = document.querySelector('#personagem-dois-nome');
+const personagemDoisEspecie = document.querySelector('#personagem-dois-especie');
+const personagemDoisStatus = document.querySelector('#personagem-dois-status');
+
+// Terceiro personagem
+const personagemTresImg = document.querySelector('#img-3');
+const personagemTresNome = document.querySelector('#personagem-tres-nome');
+const personagemTresEspecie = document.querySelector('#personagem-tres-especie');
+const personagemTresStatus = document.querySelector('#personagem-tres-status');
+
 const botao = document.querySelector('#botao');
 
 function numeroAleatorio() {
     return Math.floor(Math.random() * 826);
 }
 
-function buscaPersonagem() {
-    const personagemId = numeroAleatorio();
+function construirDados(personagens) {
+    const personagemUm = personagens[0];
+    const personagemDois = personagens[1];
+    const personagemTres = personagens[2];
 
-    return fetch(`https://rickandmortyapi.com/api/character/${personagemId}`, {
+    //Monta primeiro personagem
+    personagemUmImg.src = personagemUm.image;
+    personagemUmImg.alt = personagemUm.name;
+    personagemUmNome.innerHTML = personagemUm.name;
+    personagemUmEspecie.innerHTML = personagemUm.species;
+    personagemUmStatus.innerHTML = personagemUm.status;
+
+    //Monta segundo personagem
+    personagemDoisImg.src = personagemDois.image;
+    personagemDoisImg.alt = personagemDois.name;
+    personagemDoisNome.innerHTML = personagemDois.name;
+    personagemDoisEspecie.innerHTML = personagemDois.species;
+    personagemDoisStatus.innerHTML = personagemDois.status;
+
+    //Monta terceiro personagem
+    personagemTresImg.src = personagemTres.image;
+    personagemTresImg.alt = personagemTres.name;
+    personagemTresNome.innerHTML = personagemTres.name;
+    personagemTresEspecie.innerHTML = personagemTres.species;
+    personagemTresStatus.innerHTML = personagemTres.status;
+}
+
+function buscaPersonagem() {
+    const personagemUmId = numeroAleatorio();
+    const personagemDoisId = numeroAleatorio();
+    const personagemTresId = numeroAleatorio();
+
+    return fetch(`https://rickandmortyapi.com/api/character/${personagemUmId},${personagemDoisId},${personagemTresId}`, {
         method: 'GET',
         headers: {
             Accept: 'application/json',
             "Content-type": 'application/json'
         }
     }).then((response) => response.json()).then((data) => {
-        personagemImg.src = data.image;
-        personagemImg.alt = data.name;
-        personagemNome.innerHTML = data.name;
-        personagemEspecie.innerHTML = data.species;
-        personagemStatus.innerHTML = data.status;
+        construirDados(data);
     });
 }
 
